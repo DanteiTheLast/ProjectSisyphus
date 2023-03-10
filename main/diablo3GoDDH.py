a762235e5ea61b11d6e2486f009bfdd05ed76484
DanteiTheLast <roylinkdan@hotmail.com>
2023-03-10 07:02:03 -0600

    Functional Diablo Macro

--- main/diablo3GoDDH.py
+++ main/diablo3GoDDH.py
@@ -1,4 +1,4 @@
-import pyautogui, time, os, logging, sys, random, copy
+import pyautogui, time, os, logging, sys, random, copy, keyboard
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s.%(msecs)03d: %(message)s', datefmt='%H:%M:%S')
 #logging.disable(logging.DEBUG) #uncomment to block debug log messages
@@ -12,29 +12,40 @@ NEWTRISTRAM = 'newtristram' #this picture automatically ends the macroing if nee
 BOSSKILLED = 'bosskilled' #this picture should stop the auto clicking of numbers 1,2,3,4
 
 #Coordinates of the buttons to press
-GAME_REGION=()
+GAME_REGION=(615, 980, 300, 100)
 FINISHED = 'El programa ha lanzado los resultados' #La función para comenzar regresa este valor si es exitoso
 
 def main():
     logging.debug('Program started, press ctrl-c to abort at any time')
     logging.debug('To interrupt mouse movement, move mouse to upper left corner.')
-    startPushing()
+    seethe = imPath('seethe.png')
+    smokescreen = imPath('smokescreen.png')
+    shadowpower = imPath('shadowpower.png')
+    preparation = imPath('preparation.png')
+    startPushing(seethe, smokescreen, shadowpower, preparation)
 
 def toxico():
     pyautogui.press('enter')
     pyautogui.write('no digas mamadas, para eso mis compas', interval=0.25)
     pyautogui.press('enter')
 
-def startPushing():
-    while(pyautogui.locateOnScreen(imPath('top_right_corner.png', confidence=0.7)) is None):
-        if(pyautogui.locateOnScreen(imPath('seethe.png', confidence=1)) is not None):
+def startPushing(seethe, smokescreen, shadowpower, preparation):
+    while True:
+        pyautogui.keyDown('space')
+        pyautogui.mouseDown(button='left',duration=1)
+        pyautogui.mouseUp(button='left')
+        pyautogui.keyUp('space')
+        if(pyautogui.locateOnScreen(seethe, region=(615,980,300,100)) is not None):
             pyautogui.press('4')
-        if(pyautogui.locateOnScreen(imPath('smokescreen.png', confidence=1)) is not None):
+        if(pyautogui.locateOnScreen(smokescreen, region=(615,980,300,100)) is not None):
             pyautogui.press('3')
-        if(pyautogui.locateOnScreen(imPath('shadowpower.png', confidence=1)) is not None):
+        if(pyautogui.locateOnScreen(shadowpower, region=(615,980,300,100)) is not None):
             pyautogui.press('2')
-        if(pyautogui.locateOnScreen(imPath('preparation.png', confidence=1)) is not None):
+        if(pyautogui.locateOnScreen(preparation, region=(615,980,300,100)) is not None):
             pyautogui.press('1')
+        if(keyboard.is_pressed('esc')):
+            time.sleep(10)
+        
 
 def getGameRegion():
     """Obtains the region that the Diablo3 game is on the screen and assigns it to GAME_REGION. The game must be at the start screen (where the PLAY button is visible)."""
